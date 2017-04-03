5b848a6a80023fba68fc07593be83ba5f96dc854
b2 <brendan@mobilewebsmart.com>
2017-04-02 18:09:13 -0600

    select shorten pipe test cases ready
    3 failing tests are for Angular pipe functionality to be built next

---
+++ plugins/src/app/select-shorten-demo.component.spec.ts
@@ -0,0 +1,66 @@
+import { SelectShortenDemoComponent } from './select-shorten-demo.component';
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }           from '@angular/platform-browser';
+import { DebugElement, Component } from '@angular/core';
+
+
+describe('SelectShortenDemoComponent', function () {
+  let de: DebugElement;
+  let el: HTMLElement;
+  let comp: SelectShortenDemoComponent;
+  let fixture: ComponentFixture<SelectShortenDemoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SelectShortenDemoComponent ],
+    })
+    .compileComponents();
+  }));
+  
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SelectShortenDemoComponent);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('select'));
+    el = de.nativeElement;
+  });
+
+  it('should create component', () => {
+    fixture.detectChanges();
+    expect(comp).toBeDefined();
+  });
+
+  it('should have expected <select> css style', () => {
+    fixture.detectChanges();
+    expect(el.style['max-width']).toMatch(/350px/i,
+      '<select> should have css style: "max-width:350px;"');
+  });
+
+  it('should not shorten first option text', () => {
+    fixture.detectChanges();
+    const firstOption = el.getElementsByTagName('option')[1];
+    expect(firstOption.innerText).toMatch(/Short item text/i,
+      'first <option> should have text: "Short item text"');
+  });
+
+  it('should shorten second option text', () => {
+    fixture.detectChanges();
+    const secondOption = el.getElementsByTagName('option')[2];
+    expect(secondOption.innerText).not.toMatch(/Long item text that is too long to fit inside a select option/i,
+      'second <option> should NOT have text: "Long item text that is too long to fit inside a select option"');
+  });
+
+  it('should prepend ellipsis to second option text', () => {
+    fixture.detectChanges();
+    const secondOption = el.getElementsByTagName('option')[2];
+    expect(secondOption.innerText.substr(0, 3)).toMatch(/\.\.\./i,
+      'second <option> should have ellipsis, "..." prepended to the start of text');
+  });
+
+  it('should display complete, non-shorted option text in second option title attribute', () => {
+    fixture.detectChanges();
+    const secondOption = el.getElementsByTagName('option')[2];
+    expect(secondOption.getAttribute('title')).toMatch(/Long item text that is too long to fit inside a select option/i,
+      'second <option> should contain this value in its title attribute: "Long item text that is too long to fit inside a select option"');
+  });
+});
--- plugins/src/app/select-shorten-demo.component.ts
+++ plugins/src/app/select-shorten-demo.component.ts
@@ -2,8 +2,28 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'select-shorten-demo',
-    templateUrl: './select-shorten-demo.component.html'
+    template: `
+        <article>
+            <header>
+                <h1>Angular Pipe - Shorten Select Element</h1>
+            </header>
+            <p>
+                Option text that exceeds a select element's max-width will be truncated and ellipsis, '...' will be prepended to the beginning.
+            </p>
+            <p>
+                <i>Check it out: </i>
+                <select style="max-width:350px;">
+                    <option value="0" disabled>Please select an item</option>
+                    <option value="1">Short item text</option>
+                    <option value="2">Long item text that is too long to fit inside a select option</option>
+                    <option value="3">Short item text</option>
+                </select>
+            </p>
+            <p>
+                <b>Note:</b> Hovering your mouse over any shortened option text will reveal the complete option text in a tooltip (on browsers that support the 'title' attribute).
+            </p>
+        </article>
+    `,
+    //templateUrl: './select-shorten-demo.component.html' - For some reason templateUrl is failing in Karma tests even with beforeEach(async()) so, using inline templates until I can resolve it
 })
-export class SelectShortenDemoComponent { 
-    hello: string = 'Hello from SelectShortenDemoComponent.';
-}
+export class SelectShortenDemoComponent { }
